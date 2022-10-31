fb6cf0534b0fdbdc2cb1f4dbc366cc736ba6cb5f
Bronson <bronson.costa@outlook.com>
2022-10-31 16:02:34 -0700

    Started the beginning of Sign In fragments.

---
+++ app/src/main/java/edu/uw/tcss450/team3/tiktalk/AuthActivity.java
@@ -0,0 +1,14 @@
+package edu.uw.tcss450.team3.tiktalk;
+
+import android.os.Bundle;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+public class AuthActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_auth);
+    }
+}
