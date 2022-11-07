29cb2ab0c0395fcaab1c2716e24ed4c00c86fbd7
akannika <akannika@uw.edu>
2022-11-06 22:14:36 -0800

    Update the connection fragment with all child fragments

--- app/src/main/java/edu/uw/tcss450/team3/tiktalk/ui/connection/ConnectionFragment.java
+++ app/src/main/java/edu/uw/tcss450/team3/tiktalk/ui/connection/ConnectionFragment.java
@@ -1,12 +1,19 @@
 package edu.uw.tcss450.team3.tiktalk.ui.connection;
 
 import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
 
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.fragment.app.Fragment;
+import androidx.viewpager.widget.ViewPager;
 import androidx.viewpager2.widget.ViewPager2;
 
 import com.google.android.material.tabs.TabLayout;
+
+import edu.uw.tcss450.team3.tiktalk.R;
+import edu.uw.tcss450.team3.tiktalk.adapter.ViewPagerAdapter;
 
 /**
  * A simple {@link Fragment} subclass.
@@ -14,7 +21,7 @@ import com.google.android.material.tabs.TabLayout;
 public class ConnectionFragment extends Fragment {
 
     TabLayout tabLayout;
-    ViewPager2 viewPager2;
+    ViewPager viewPager;
 
     public ConnectionFragment() {
         // Required empty public constructor
@@ -24,37 +31,26 @@ public class ConnectionFragment extends Fragment {
     @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-//        setContentView(R.layout.fragment_connection);
+    }
 
-//        tabLayout.findViewById(R.id.tab_layout);
-//        viewPager2.findViewById(R.id.view_pager);
-//        adapter = new Adapter(this);
-//        viewPager2.setAdapter(adapter);
-//
-//        tabLayout.addOnTabSelectedListener(new TabLayout.OnTabSelectedListener() {
-//            @Override
-//            public void onTabSelected(TabLayout.Tab tab) {
-//                viewPager2.setCurrentItem(tab.getPosition());
-//            }
-//
-//            @Override
-//            public void onTabUnselected(TabLayout.Tab tab) {
-//
-//            }
-//
-//            @Override
-//            public void onTabReselected(TabLayout.Tab tab) {
-//
-//            }
-//        });
-//
-//        viewPager2.registerOnPageChangeCallback(new ViewPager2.OnPageChangeCallback() {
-//            @Override
-//            public void onPageSelected(int position) {
-//                super.onPageSelected(position);
-//                tabLayout.getTabAt(position).select();
-//            }
-//        });
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        // Inflate the layout for this fragment
+        View view = inflater.inflate(R.layout.fragment_connection, container, false);
+        addFragment(view);
+        return view;
+    }
 
+    private void addFragment(View view) {
+        tabLayout = view.findViewById(R.id.connectionTabLayout);
+        viewPager = view.findViewById(R.id.viewPager);
+        ViewPagerAdapter adapter = new ViewPagerAdapter(getChildFragmentManager());
+        adapter.addFragment(new ContactFragment(), "Contact");
+        adapter.addFragment(new AddFriendFragment(), "Add");
+        adapter.addFragment(new RequestInFragment(), "Request");
+        adapter.addFragment(new RequestOutFragment(), "Sent");
+        viewPager.setAdapter(adapter);
+        tabLayout.setupWithViewPager(viewPager);
     }
 }
