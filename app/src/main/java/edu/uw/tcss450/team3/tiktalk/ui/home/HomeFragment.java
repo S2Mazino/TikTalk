5278c57eddcfa0af35ee396704077da69eeea27a
Bronson <bronson.costa@outlook.com>
2022-11-14 22:13:15 -0800

    temp nickname display

--- app/src/main/java/edu/uw/tcss450/team3/tiktalk/ui/home/HomeFragment.java
+++ app/src/main/java/edu/uw/tcss450/team3/tiktalk/ui/home/HomeFragment.java
@@ -2,7 +2,11 @@ package edu.uw.tcss450.team3.tiktalk.ui.home;
 
 import android.os.Bundle;
 
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
 import androidx.fragment.app.Fragment;
+import androidx.lifecycle.ViewModel;
+import androidx.lifecycle.ViewModelProvider;
 import androidx.navigation.Navigation;
 
 import android.view.LayoutInflater;
@@ -10,13 +14,18 @@ import android.view.View;
 import android.view.ViewGroup;
 
 import edu.uw.tcss450.team3.tiktalk.R;
+import edu.uw.tcss450.team3.tiktalk.databinding.FragmentHomeBinding;
 import edu.uw.tcss450.team3.tiktalk.databinding.FragmentSignInBinding;
+import edu.uw.tcss450.team3.tiktalk.model.UserInfoViewModel;
 import edu.uw.tcss450.team3.tiktalk.ui.auth.signin.SignInFragmentDirections;
 
 /*
  * A simple {@link Fragment} subclass.
  */
 public class HomeFragment extends Fragment {
+
+    private FragmentHomeBinding binding;
+    private UserInfoViewModel mUserModel;
 
     public HomeFragment() {
         // Required empty public constructor
@@ -33,7 +42,19 @@ public class HomeFragment extends Fragment {
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
+        binding = FragmentHomeBinding.inflate(inflater);
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_home, container, false);
+        return binding.getRoot();
     }
+
+    @Override
+    public void onViewCreated(@NonNull View view, @Nullable Bundle savedInstanceState) {
+        super.onViewCreated(view, savedInstanceState);
+
+        mUserModel = new ViewModelProvider(getActivity()).get(UserInfoViewModel.class);
+        binding.nickName.setText(mUserModel.getEmail());
+
+    }
+
+
 }
